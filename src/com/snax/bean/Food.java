04aeac353cb41c67d67eb5e50991c7df58b5d1f9
baruas1 <sumeet.barua@vce.com>
2017-08-22 00:46:02 +0530

    setter/getter added

--- src/com/snax/bean/Food.java
+++ src/com/snax/bean/Food.java
@@ -1,5 +1,15 @@
 package com.snax.bean;
 
 public class Food {
+  private String foodId;
 
+public String getFoodId() {
+	return foodId;
+}
+
+public void setFoodId(String foodId) {
+	this.foodId = foodId;
+}
+  
+  
 }
