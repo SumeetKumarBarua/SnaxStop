fe3705b00621a6a954309102bedb9f218afcbea7
Debasis <devsahoo93@gmail.com>
2017-08-22 01:34:03 +0530

    Added password

--- src/com/snax/bean/Login.java
+++ src/com/snax/bean/Login.java
@@ -2,6 +2,15 @@ package com.snax.bean;
 
 public class Login {
  private String userId;
+ private String password;
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
 
 public String getUserId() {
 	return userId;
