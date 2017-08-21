e1da05c3f6173b5d6356d807c60c4df241fe481d
Debasis <devsahoo93@gmail.com>
2017-08-22 01:20:33 +0530

    trying

---
+++ src/com/snax/bean/Login.java
@@ -0,0 +1,14 @@
+package com.snax.bean;
+
+public class Login {
+ private String userId;
+
+public String getUserId() {
+	return userId;
+}
+
+public void setUserId(String userId) {
+	this.userId = userId;
+}
+ 
+}
